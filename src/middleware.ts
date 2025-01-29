f17b734ec87cb16f5c8861a04e3e8aedc2401cf7
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-29 10:34:35 +0530

    Few changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,12 @@
-import { NextResponse } from "next/server";
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware((auth, req) => {
-    const { userId } = auth();
-
-    // If user is signed in and tries to access home page
-    if (userId && req.nextUrl.pathname === "/") {
-        return NextResponse.rewrite(new URL("/", req.url));
-    }
-
-    // For all other routes, continue normally
-    return NextResponse.next();
-});
+export default clerkMiddleware();
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
